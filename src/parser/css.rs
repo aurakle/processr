be1008a5a0cd25288cfca9de27dddf69bbe9e663
Aurora Dawn <aurora.dawn@trans.fish>
2025-07-05 08:32:54 -0400

    Yay

---
+++ src/parser/css.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use anyhow::Result;
+
+use crate::Meta;
+
+use super::ParserProcedure;
+
+#[derive(Clone)]
+pub struct CssCompressor();
+
+impl CssCompressor {
+    pub fn default() -> CssCompressor {
+        CssCompressor()
+    }
+}
+
+impl ParserProcedure for CssCompressor {
+    fn process(&self, bytes: &Vec<u8>, properties: &HashMap<String, Meta>) -> Result<(Vec<u8>, HashMap<String, Meta>)> {
+        let mut text = String::from_utf8(bytes.clone())?.replace("\n", "");
+        let mut last_len = text.len() * 2;
+
+        while text.len() < last_len {
+            last_len = text.len();
+            text = text.replace("	", " ");
+            text = text.replace("  ", " ");
+        }
+
+        Ok((text.as_bytes().to_vec(), properties.clone()))
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,6 +9,7 @@ use super::SingleProcedure;
 
 pub mod markdown;
 pub mod template;
+pub mod css;
 
 pub trait ParserProcedure: Clone {
     fn process(&self, bytes: &Vec<u8>, properties: &HashMap<String, Meta>) -> Result<(Vec<u8>, HashMap<String, Meta>)>;
