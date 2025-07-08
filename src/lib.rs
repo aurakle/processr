c355c398c64f9bcd8e5c715e841a56ddc1e8bb0a
Aurora Dawn <aurora.dawn@trans.fish>
2025-07-07 20:33:38 -0400

    This might be nicer

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@ macro_rules! processr {
                 $crate::clean($out)?
             }
 
-            $(let $names = $rules; $crate::procedure::Procedure::write(&$names, $out)?;)+
+            $(let $names = $rules; $crate::anyhow::Context::context($crate::procedure::Procedure::write(&$names, $out), "While evaluating rule $names")?;)+
 
             Ok(())
         }
