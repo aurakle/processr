c084f80c7babc689bacd53ca8dc90d1f7e9b703d
Aurora Dawn <aurora.dawn@trans.fish>
2025-07-10 19:59:43 -0400

    Teehee

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,5 +2,5 @@ pub use crate::processr;
 pub use crate::create;
 pub use crate::selector::{exact, regex, wild};
 pub use crate::procedure::{SingleProcedure, MultiProcedure};
-pub use crate::parser::{markdown::MarkdownParser, html::HtmlParser, css::CssParser, template::TemplateParser, image::ImageConverter};
+pub use crate::parser::{ParserProcedure, markdown::MarkdownParser, html::HtmlParser, css::CssParser, template::TemplateParser, image::ImageConverter};
 pub use crate::data::Value;
