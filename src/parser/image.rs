4a848e237a265c26d48a969228215351c6dd4b93
Aurora Dawn <aurora.dawn@trans.fish>
2025-07-10 19:55:52 -0400

    YEEH AW

--- src/parser/image.rs
+++ src/parser/image.rs
@@ -9,18 +9,26 @@ use crate::data::{Item, State};
 use super::ParserProcedure;
 
 #[derive(Clone)]
-pub struct ToWebpConverter();
+pub struct ImageConverter {
+    format: ImageFormat,
+}
+
+impl ImageConverter {
+    fn new(format: ImageFormat) -> Self {
+        Self { format }
+    }
+}
 
 #[async_trait(?Send)]
-impl ParserProcedure for ToWebpConverter {
+impl ParserProcedure for ImageConverter {
     fn default() -> Self {
-        Self()
+        Self::new(ImageFormat::WebP)
     }
 
     async fn process(&self, state: &mut State, item: &Item) -> Result<Item> {
         let img = ImageReader::new(Cursor::new(item.bytes.clone())).with_guessed_format()?.decode()?;
         let mut bytes = Vec::new();
-        img.write_to(&mut Cursor::new(&mut bytes), ImageFormat::WebP)?;
+        img.write_to(&mut Cursor::new(&mut bytes), self.format.clone())?;
 
         Ok(Item {
             bytes,
